57dd61339a075ce0932f7cd2b220e808cbb2cf98
Markus Weber <markus.weber@wacom.com>
2024-01-11 07:58:00 +0100

    Add new package to __all__

--- knowledge/__init__.py
+++ knowledge/__init__.py
@@ -17,7 +17,7 @@ __license__ = "Wacom"
 __maintainer__ = ["Markus Weber"]
 __email__ = "markus.weber@wacom.com"
 __status__ = "beta"
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 # Create the Logger
 logger: Union[logging.Logger, None] = None
