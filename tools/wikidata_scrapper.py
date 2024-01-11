5a14f38fa39c4614db938dfed630e7633e72402f
Markus Weber <markus.weber@wacom.com>
2024-01-11 07:56:45 +0100

    Moving helper functions for Wikidata into separate package.

--- tools/wikidata_scrapper.py
+++ tools/wikidata_scrapper.py
@@ -11,9 +11,8 @@ import ndjson
 from tqdm import tqdm
 
 from knowledge import logger
-from knowledge.base.entity import LanguageCode, IMAGE_TAG, STATUS_FLAG_TAG, Description, Label
-from knowledge.base.language import LANGUAGE_LOCALE_MAPPING, LocaleCode
-from knowledge.base.ontology import ThingObject, OntologyContext, OntologyClassReference
+from knowledge.base.language import LANGUAGE_LOCALE_MAPPING
+from knowledge.base.ontology import ThingObject, OntologyContext
 from knowledge.ontomapping import get_mapping_configuration, register_ontology, PropertyConfiguration, PropertyType, \
     load_configuration, ClassConfiguration
 from knowledge.ontomapping.manager import wikidata_to_thing, wikidata_taxonomy
@@ -21,14 +20,7 @@ from knowledge.public.relations import wikidata_relations_extractor
 from knowledge.public.wikidata import WikiDataAPIClient, WikidataThing, WikidataClass
 from knowledge.services.graph import WacomKnowledgeService
 from knowledge.services.ontology import OntologyService
-
-
-# -------------------------------------------------- Structures --------------------------------------------------------
-class WikidataSyncException(Exception):
-    """
-    Exception class for Wikidata synchronization.
-    """
-
+from knowledge.utils.wikidata import build_query, extract_qid
 
 # -------------------------------------------------- Structures --------------------------------------------------------
 WACOM_KNOWLEDGE_TAG: str = 'wacom-knowledge'
@@ -37,174 +29,6 @@ DEFAULT_TYPE_TAG: str = "default-type"
 ENTITY_LIST_MODE: str = 'entity-list'
 SPARQL_QUERY_MODE: str = 'query'
 CLASS_MAPPING: str = 'class-mapping'
-
-
-# ----------------------------------------------- Helper functions -----------------------------------------------------
-def update_language_code(lang: LanguageCode) -> LocaleCode:
-    """ Update the language_code code to a default language_code / country code
-    Parameters
-    ----------
-    lang: LanguageCode
-        Language code.
-
-    Returns
-    -------
-    language_code: LocaleCode
-        Language code.
-
-    Raises
-    ------
-    ValueError
-        If the language_code code is not supported.
-    """
-    if lang not in LANGUAGE_LOCALE_MAPPING:
-        raise ValueError(f'Language code {lang} not supported.')
-    return LANGUAGE_LOCALE_MAPPING[lang]
-
-
-def localized_list_description(entity_dict: Dict[str, str]) -> List[Description]:
-    """
-    Creates a list of descriptions for the given entity dictionary.
-    Parameters
-    ----------
-    entity_dict: Dict[str, str]
-        Entity dictionary.
-
-    Returns
-    -------
-    descriptions: List[Description]
-        List of descriptions.
-    """
-    return [Description(cont, update_language_code(LanguageCode(lang))) for lang, cont in entity_dict.items()]
-
-
-def localized_list_label(entity_dict: Dict[str, str]) -> List[Label]:
-    """
-    Creates a list of labels for the given entity dictionary.
-
-    Parameters
-    ----------
-    entity_dict: Dict[str, str]
-        Entity dictionary.
-
-    Returns
-    -------
-    labels: List[Label]
-        List of labels.
-    """
-    return [Label(cont, update_language_code(LanguageCode(lang)), main=True)
-            for lang, cont in entity_dict.items() if cont != '']
-
-
-def localized_flatten_alias_list(entity_dict: Dict[str, List[str]]) -> List[Label]:
-    """
-    Flattens the alias list.
-    Parameters
-    ----------
-    entity_dict: Dict[str, List[str]]
-        Entity dictionary.
-
-    Returns
-    -------
-    flatten: List[Label]
-        Flattened list of labels.
-    """
-    flatten: List[Label] = []
-    for language, items in entity_dict.items():
-        for i in items:
-            if i != '':
-                flatten.append(Label(i, update_language_code(LanguageCode(language)), main=False))
-    return flatten
-
-
-def from_dict(entity: Dict[str, Any], concept_type: OntologyClassReference) -> ThingObject:
-    """
-    Create a thing object from a dictionary.
-    Parameters
-    ----------
-    entity: Dict[str, Any]
-        Entity dictionary.
-    concept_type: OntologyClassReference
-        Concept type.
-
-    Returns
-    -------
-    thing: ThingObject
-        Thing object.
-    """
-    labels: List[Label] = localized_list_label(entity['label'])
-    description: List[Description] = localized_list_description(entity['description'])
-    alias: List[Label] = localized_flatten_alias_list(entity['alias'])
-    if IMAGE_TAG in entity:
-        icon: str = entity[IMAGE_TAG]
-    else:
-        logger.warning(f"Entity has no image: {entity}")
-        icon: str = ''
-    # Create the entity
-    thing: ThingObject = ThingObject(label=labels, concept_type=concept_type, description=description, icon=icon)
-    thing.alias = alias
-    if STATUS_FLAG_TAG in entity:
-        thing.status_flag = entity[STATUS_FLAG_TAG]
-    return thing
-
-
-# --------------------------------------------------- Utilities --------------------------------------------------------
-
-def strip(url: str) -> str:
-    """Strip qid from url.
-    Parameters
-    ----------
-    url: str
-        URL
-    Returns
-    -------
-    result: str
-        Stripped URL
-    """
-    parts = url.split('/')
-    return parts[-1]
-
-
-def build_query(params: Dict[str, Any]) -> List[str]:
-    """
-    Build of query.
-
-    Parameters
-    ----------
-    params:
-        Parameters for query
-
-    Returns
-    -------
-    queries: List[str]
-        SPARQL query string
-    """
-    filters: List[Dict[str, Any]] = params.get('filters')
-    dynamics: Dict[str, Any] = params.get('dynamic-filters')
-    limit: int = params.get('limit', 1000)
-    lang_code: str = params.get('language_code', 'en')
-    filter_string: str = ''
-    queries: List[str] = []
-    for f in filters:
-        filter_string += f"?item wdt:{f['property']}  wd:{f['target']}.\n"
-    if dynamics:
-        property_str: str = dynamics["property"]
-        for v in dynamics["targets"]:
-            dyn: str = filter_string + f"?item wdt:{property_str}  wd:{v}.\n"
-            query: str = f"""SELECT DISTINCT ?item ?itemLabel WHERE {{
-              {dyn}SERVICE wikibase:label {{ bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],{lang_code}\". }}
-            }}
-            LIMIT {limit}
-            """
-            queries.append(query)
-    else:
-        query: str = f"""SELECT DISTINCT ?item ?itemLabel WHERE {{
-          {filter_string}SERVICE wikibase:label {{ bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],{lang_code}\". }}
-        }}
-        LIMIT {limit}
-        """
-        queries.append(query)
-    return queries
 
 
 def load_cache(cache: Path) -> Dict[str, WikidataThing]:
@@ -278,22 +102,6 @@ def check_missing_qids(entities: List[WikidataThing]) -> Set[str]:
                                 missing.add(qid)
     return missing
 
-
-def extract_qid(url: str) -> str:
-    """
-    Extract qid from url.
-    Parameters
-    ----------
-    url: str
-        URL
-
-    Returns
-    -------
-    qid: str
-        QID
-    """
-    parts: List[str] = url.split('/')
-    return parts[-1]
 
 
 def main(mapping: Path, cache: Path, languages: List[str] = None, max_depth: int = -1):
