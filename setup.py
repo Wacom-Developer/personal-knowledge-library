72f0d4fbd689bc3060375ce12913dd9a3971efbd
Markus Weber <markus.weber@wacom.com>
2024-12-09 15:24:38 +0100

    Update supported versions.

--- setup.py
+++ setup.py
@@ -68,10 +68,10 @@ setup(
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Operating System :: OS Independent",
         "Natural Language :: English",
