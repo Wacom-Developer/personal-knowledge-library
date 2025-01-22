69a4c22054b7f62211e570f19be659d20cb92be8
Markus Weber <markus.weber@wacom.com>
2025-01-22 19:07:44 +0100

    [refactoring] - Refactor the Tenant client to use the same parameter style for timeout and retry mechanisms.

--- knowledge/base/tenant.py
+++ knowledge/base/tenant.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright © 2024-present Wacom. All rights reserved.
-from typing import List
+from typing import List, Dict, Any
 
 
 class TenantConfiguration:
@@ -28,7 +28,7 @@ class TenantConfiguration:
     is_locked: bool
         Flag to indicate if the tenant is locked
     name: str
-        name of the tenant
+        Name of the tenant
     rights: List[str]
         List of rights
     vector_search_data_properties: List[str]
@@ -45,34 +45,57 @@ class TenantConfiguration:
                  vector_search_object_properties: List[str],
                  content_data_property_name: str):
         # Constructor to initialize the properties
-        self.__identifier = identifier
-        self.__ontology_name = ontology_name
-        self.__ontology_version = ontology_version
-        self.__is_locked = is_locked
-        self.__name = name
-        self.__rights = rights
-        self.__vector_search_data_properties = vector_search_data_properties
-        self.__vector_search_object_properties = vector_search_object_properties
-        self.__content_data_property_name = content_data_property_name
+        self.__identifier: str = identifier
+        self.__ontology_name: str = ontology_name
+        self.__ontology_version: int = ontology_version
+        self.__is_locked: bool = is_locked
+        self.__name: str = name
+        self.__rights: List[str] = rights
+        self.__vector_search_data_properties: List[str] = vector_search_data_properties
+        self.__vector_search_object_properties: List[str] = vector_search_object_properties
+        self.__content_data_property_name: str = content_data_property_name
 
     @property
-    def identifier(self):
+    def identifier(self) -> str:
+        """
+        Identifier of the tenant
+        Returns
+        -------
+        str
+            Identifier of the tenant
+        """
         return self.__identifier
 
     @property
-    def ontology_name(self):
+    def ontology_name(self) -> str:
+        """
+        Name of the ontology.
+        Returns
+        -------
+        str
+            Name of the ontology.
+        """
         return self.__ontology_name
 
     @property
-    def ontology_version(self):
+    def ontology_version(self) -> int:
+        """
+        Version of the ontology.
+        """
         return self.__ontology_version
 
     @property
-    def is_locked(self):
+    def is_locked(self) -> bool:
+        """
+        Flag to indicate if the tenant is locked.
+        """
         return self.__is_locked
 
     @property
-    def name(self):
+    def name(self) -> str:
+        """
+        Name of the tenant.
+        """
         return self.__name
 
     @name.setter
@@ -81,6 +104,9 @@ class TenantConfiguration:
 
     @property
     def rights(self):
+        """
+        List of rights being assigned to the tenant, and will be added to the user's rights in the token.
+        """
         return self.__rights
 
     @rights.setter
@@ -88,7 +114,10 @@ class TenantConfiguration:
         self.__rights = value
 
     @property
-    def vector_search_data_properties(self):
+    def vector_search_data_properties(self) -> List[str]:
+        """
+        List of vector search data properties which are used for vector search in the metadata.
+        """
         return self.__vector_search_data_properties
 
     @vector_search_data_properties.setter
@@ -97,6 +126,9 @@ class TenantConfiguration:
 
     @property
     def vector_search_object_properties(self):
+        """
+        List of vector search object properties which are used for vector search in the metadata.
+        """
         return self.__vector_search_object_properties
 
     @vector_search_object_properties.setter
@@ -105,6 +137,9 @@ class TenantConfiguration:
 
     @property
     def content_data_property_name(self):
+        """
+        Name of the content data property which is used for vector search to index documents.
+        """
         return self.__content_data_property_name
 
     @content_data_property_name.setter
@@ -112,7 +147,20 @@ class TenantConfiguration:
         self.__content_data_property_name = value
 
     @classmethod
-    def from_dict(cls, data_dict):
+    def from_dict(cls, data_dict: Dict[str, Any]) -> 'TenantConfiguration':
+        """
+        Create a TenantConfiguration object from a dictionary.
+
+        Parameters
+        ----------
+        data_dict: Dict[str, Any]
+            Dictionary containing the tenant configuration data.
+
+        Returns
+        -------
+        TenantConfiguration
+            The tenant configuration object.
+        """
         return cls(
             identifier=data_dict.get('id'),
             ontology_name=data_dict.get('ontologyName'),
