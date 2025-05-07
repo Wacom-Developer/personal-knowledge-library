303dfa95c8f61a5d455fbacfab96028ed667ab4a
Markus Weber <Markus.Weber@wacom.com>
2025-05-07 21:25:41 +0200

    [feature] Add more constants for the import status.

--- knowledge/base/response.py
+++ knowledge/base/response.py
@@ -39,13 +39,18 @@ class JobStatus:
     finished_at: Optional[datetime]
         The timestamp when the job finished.
     """
+
     PENDING: str = "Pending"
     """The job is pending. It has not started yet, as another job is in progress."""
     IN_PROGRESS: str = "InProgress"
     """The job is in progress. It has started but not yet completed."""
     COMPLETED: str = "Completed"
     """The job is completed."""
-    
+    FAILED: str = "Failed"
+    """The job has failed."""
+    RETRYING: str = "Retrying"
+    """The job is being retried."""
+
     def __init__(
         self,
         user_id: str,
